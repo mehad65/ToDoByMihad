13d5878171b07e7fc8145fff0338d9ab27c4c6b2
mehad65 <mehad65@gmail.com>
2022-07-17 02:43:04 +0600

    complete todo inmemory data hold

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -12,10 +12,21 @@ export class TodoComponent implements OnInit {
   todoList:ITask[]=[];
   inProgressList:ITask[]=[];
   doneList:ITask[]=[];
+
+  holdIndex!:number;
+  isUpdate:boolean=false;
   constructor(private fb:FormBuilder)
   {
     
   }
+  
+  ngOnInit(): void 
+  {
+    this.todoForm=this.fb.group({
+      item:["",Validators.required]
+    })
+  }
+  //functions is here
   drop(event: CdkDragDrop<ITask[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
@@ -28,11 +39,31 @@ export class TodoComponent implements OnInit {
       );
     }
   }
-  ngOnInit(): void 
-  {
-    this.todoForm=this.fb.group({
-      item:["",Validators.required]
+ EditTask(index:number){
+    this.todoForm.controls["item"].setValue(this.todoList[index].description);
+    this.isUpdate=true;
+    this.holdIndex=index;
+ }
+ UpdateTask(index:number){
+    this.todoList[this.holdIndex].description=this.todoForm.value.item;
+    this.isUpdate=false;
+    this.todoForm.reset();
+ }
+ DeleteTask(index:number){
+  this.todoList.splice(index,1);
+ }
+ DeleteInProgressTask(index:number){
+  this.inProgressList.splice(index,1);
+ }
+ DeleteDoneTask(index:number){
+    this.doneList.splice(index,1);
+ }
+  AddTask(){
+    this.todoList.push({
+      description:this.todoForm.value.item,
+      done:false
     })
+    this.todoForm.reset();
   }
 
 }
